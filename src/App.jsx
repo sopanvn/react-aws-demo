3a1e2cafe199cc8653a4fd1dcac9f6c2563f11e0
sopanvn <nadagudesopan@gmail.com>
2026-02-26 19:00:48 +0530

    Testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,9 @@
-import { useState,useEffect } from 'react'
+import { useState } from 'react'
 import './App.css'
 
-function App() {
-  
-  const [data,setData] = useState([]);
-  const [loading,setLoading] =useState(false);
-  const [error,setError] =useState('');
-    //const [refetchCount, setRefetchCount] = useState(0);
+function App()
+{
     const [tempData, setTempData] = useState("I love you my Jana, My Dear wife Sakshi 💖 ");
-  
-
-  useEffect(()=>{
-
-    const controller=new AbortController(); 
-
-    const fetchData = async ()=>{
-
-    try
-    { 
-        setLoading(true);
-        setError("");
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts',{signal:controller.signal});
-        if(!response.ok)
-        {
-          throw new Error('API Failed');
-        }
-        const api_data= await response.json();
-
-        setData(api_data.slice(0,100));
-
-    }
-    catch(err)
-    {
-      if(err.name!=='AbortError')
-      {
-        setError(err.message);
-      }
-    }
-    finally
-    {
-      setTimeout(setLoading(false),2000);
-    }
-  }
-
-  fetchData();
-
-
-  return ()=>{
-
-      controller.abort();
-
-  }
-
-  }, [])
-
-
     const acceptHandler = () => {
         setTempData("I Love You Too 💖");
     }
@@ -65,13 +14,7 @@ function App() {
   return (
     <>  
           <button onClick={() => { acceptHandler } }> Accept </button>      
-        {loading && <p style={{color:"white"}}>Loading...</p>}
-        {error && <p>{error}</p>}
-        { <ul>
-          {data.map((user)=>{
-            return <li key={user.id}> {user.title} </li>
-          })}
-        </ul> }
+        
           <span>{tempData}</span>
     </>
   )
